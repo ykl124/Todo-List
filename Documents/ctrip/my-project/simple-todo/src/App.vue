3351f84f48a90bdca1bc5ff20502a4538f7e8609
yangkaili <yangkaili@f-road.com.cn>
2017-09-14 16:57:04 +0800

    simple-todo by vue-cli

---
+++ Documents/ctrip/my-project/simple-todo/src/App.vue
@@ -0,0 +1,28 @@
+<template>
+  <div id="app">
+    <GetTodo></GetTodo>
+    <CurrentTodos></CurrentTodos>
+    <CompletedTodos></CompletedTodos>
+  </div>
+</template>
+
+<script>
+  import GetTodo from './components/GetTodo.vue'
+  import CurrentTodos from './components/CurrentTodos.vue'
+  import CompletedTodos from './components/CompletedTodos.vue'
+
+  export default {
+    components: {
+      GetTodo,
+      CurrentTodos,
+      CompletedTodos
+    }
+  }
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  margin: 5px;
+}
+</style>
